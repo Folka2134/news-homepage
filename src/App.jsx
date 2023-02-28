df15a52ee66b9fa2de1aaddf11e37fc42e221245
Folka2134 <folka2134@gmail.com>
2023-02-28 11:21:11 +0000

    created basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,63 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div></div>
+    <div className="App bg-white">
+      <div className="border-2 border-black m-6">
+        <nav>
+          <ul></ul>
+        </nav>
+        <main>
+          <div>
+            <img src="" alt="banner" />
+            <h1>The Bright Future of Web 3.0?</h1>
+            <div>
+              <p>
+                We dive into the next evolution of the web that claims to put
+                the power of the platforms back into the hands of the people.
+                But is it really fulfilling its promise?
+              </p>
+              <button>Read more</button>
+            </div>
+          </div>
+          <div>
+            <h1>New</h1>
+            <p>
+              Hydrogen VS Electric Cars Will hydrogen-fueled cars ever catch up
+              to EVs?
+            </p>
+            <p>
+              The Downsides of AI Artistry What are the possible adverse effects
+              of on-demand AI image generation?
+            </p>
+            <p>
+              Is VC Funding Drying Up? Private funding by VC firms is down 50%
+              YOY. We take a look at what that means.
+            </p>
+          </div>
+        </main>
+        <div>
+          <ul>
+            <div>
+              <img src="" alt="" />
+              <h3>01</h3>
+              <h4>Reviving Retro PCs</h4>
+              <p>What happens when old PCs are given modern upgrades?</p>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <h3>02</h3>
+              <h4>Top 10 Laptops of 2022</h4>
+              <p>Our best picks for various needs and budgets.</p>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <h3>03</h3>
+              <h4>The Growth of Gaming</h4>
+              <p>How the pandemic has sparked fresh opportunities.</p>
+            </div>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
